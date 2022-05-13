d340b0606dba76a0ae2a15b1072ab3c3eb5c7abe
Iyin0 <p2iyinoluwa@gmail.com>
2022-05-13 18:25:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import Body from "./Body";
 import Navbar from "./Navbar";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
       <BrowserRouter>
         <Navbar/>
-        <Routes>
-          <Route path="/sneakers" element={<Body/>} />
-        </Routes>
+        <Body/>
       </BrowserRouter> 
     </div>
   );
